c2c8114c84e4e532be186c4ac2ec55a0a7900848
Sebastian Schäfer <bschr9uwb@googlemail.com>
2019-05-06 10:22:25 +0200

    Add filter.js

---
+++ arrays/filter.js
@@ -0,0 +1,19 @@
+let products = [
+    { name: 'cucumber', type: 'vegetable', quantity: 0, price: 5 },
+    { name: 'banana', type: 'fruit', quantity: 10, price: 54 },
+    { name: 'orange', type: 'fruit', quantity: 30, price: 15 },
+    { name: 'chocolate', type: 'sweets', quantity: 25, price: 7 }
+]
+
+let filteredProductsOneArg = products.filter((product) => {
+    return product.type === 'fruit'
+})
+console.log(filteredProductsOneArg);
+
+
+let filteredProducts = products.filter((product) => {
+    return product.type === 'fruit'
+        && product.quantity > 0
+        && product.price < 30
+})
+console.log(filteredProducts)
