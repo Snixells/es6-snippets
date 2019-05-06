5bb12004457b13fb2b98e4eacaebcaf8bc4f3773
Sebastian Schäfer <bschr9uwb@googlemail.com>
2019-05-06 13:51:13 +0200

    Add every-some.js

---
+++ arrays/every-some.js
@@ -0,0 +1,21 @@
+// -- Example 1 -----------------------
+let computers = [
+    { name: 'Mac', ram: 2 },
+    { name: 'Surface', ram: 32 },
+    { name: 'Desktop', ram: 64 }
+];
+
+let canEveryComputerRunProgram = computers.every((computer) => {
+    return computer.ram > 16
+})
+
+let canOnlySomeComputersRunProgram = computers.some((computer) => {
+    return computer.ram > 16
+})
+
+console.log(canEveryComputerRunProgram);
+console.log(canOnlySomeComputersRunProgram);
+
+
+
+// -- Example 2 -----------------------
