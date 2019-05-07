23d6c95f263673dbf9f1443315b8729a1affa6c5
Sebastian Schäfer <bschr9uwb@googlemail.com>
2019-05-07 16:12:10 +0200

    Add arrow-functins.js

---
+++ arrow-functions.js
@@ -0,0 +1,24 @@
+// -- Example 1 -----------------------
+const add = (a, b) => {
+    return a + b;
+}
+console.log(add(5, 5));
+
+
+
+// -- Example 2 -----------------------
+const multiply = (a, b) => a * b;
+console.log(multiply(5, 5));
+
+
+
+// -- Example 3 -----------------------
+const double = x => x * 2
+console.log(double(5));
+
+
+
+// -- Example 4 -----------------------
+const numbers = [1, 2, 3];
+let doubledNumbers = numbers.map(number => number * 2)
+console.log(doubledNumbers);
